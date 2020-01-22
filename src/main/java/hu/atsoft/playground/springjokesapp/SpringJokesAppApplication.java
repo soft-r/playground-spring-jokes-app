21aba3c288f422ffa3bebff6a4072a128baf73ac
dp <hudomokp@gmail.com>
2020-01-22 21:22:31 +0100

    Initial commit

---
+++ src/main/java/hu/atsoft/playground/springjokesapp/SpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package hu.atsoft.playground.springjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/atsoft/playground/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.atsoft.playground.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
